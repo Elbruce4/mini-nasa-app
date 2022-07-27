99d4f2c77e299944855b59736296c83e948dbd4b
Elbruce4 <78322819+Elbruce4@users.noreply.github.com>
2022-07-27 20:08:30 -0300

    show desc of photos

--- lib/components/photoWidget.dart/index.dart
+++ lib/components/photoWidget.dart/index.dart
@@ -28,7 +28,7 @@ class _PhotoWidgetState extends State<PhotoWidget> {
   var _progress = 0;
   bool isDownloading = false;
   bool loading = false;
-
+  bool showDesc = false;
 
   widgetInit () async {
     setState(() {
@@ -95,109 +95,148 @@ class _PhotoWidgetState extends State<PhotoWidget> {
   Widget build(BuildContext context) {
 
 
-    return Container(
-      decoration: BoxDecoration(
-        image: args != null ? DecorationImage(
-          image: NetworkImage(args["url"]),
-          fit: BoxFit.fitWidth
-        )
-        :
-        null
-      ),
-      height: 700,
-      width: 400,
-      margin: EdgeInsets.only(
-        top: 30
-      ),
-      child: LoadingOverlay(
-        color: Colors.black,
-        opacity: 0.8,
-        progressIndicator: CircularProgressIndicator(
-          color: Color.fromARGB(255, 24, 40, 187),
-          strokeWidth: 6,
+    return GestureDetector(
+      onTap: (){
+        setState(() {
+          showDesc = !showDesc;
+        });
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          image: args != null ? DecorationImage(
+            image: NetworkImage(args["url"]),
+            fit: BoxFit.fitWidth
+          )
+          :
+          null
         ),
-        isLoading: loading,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children:[
-            Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Card(
-                color: Colors.black,
-                child: IconButton(
-                    color: Colors.black,
-                    icon: Icon(
-                      Icons.arrow_back_ios_new_rounded,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-              ),
-              Card(
-                color: Colors.black,
-                child: IconButton(
-                    color: Colors.black,
-                    icon: Icon(
-                      Icons.ios_share_outlined,
-                      color: Colors.white,
-                    ),
-                    onPressed: () async {
-                      await Share.share(args["url"]);
-                    },
-                  ),
-              ),
-              Card(
-                color: Colors.black,
-                child: IconButton(
-                    color: Colors.black,
-                    icon: Icon(
-                      Icons.arrow_circle_down,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      image_Downlader(args["url"] , context);
-                    },
-                  ),
-              )
-            ],
+        height: 700,
+        width: 400,
+        margin: EdgeInsets.only(
+          top: 30
+        ),
+        child: LoadingOverlay(
+          color: Colors.black,
+          opacity: 0.8,
+          progressIndicator: CircularProgressIndicator(
+            color: Color.fromARGB(255, 24, 40, 187),
+            strokeWidth: 6,
           ),
-          if(_progress < 100) 
-          Column(
-            children: [
-              Visibility(
-                visible: isDownloading,
-                child: Material(
-                  type: MaterialType.transparency,
-                  child: Text(
-                    "Downloading, This could take a few seconds",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 13
-                  )),
-                ),
+          isLoading: loading,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children:[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Card(
+                    color: Colors.black,
+                    child: IconButton(
+                        color: Colors.black,
+                        icon: Icon(
+                          Icons.arrow_back_ios_new_rounded,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                  ),
+                  Card(
+                    color: Colors.black,
+                    child: IconButton(
+                        color: Colors.black,
+                        icon: Icon(
+                          Icons.ios_share_outlined,
+                          color: Colors.white,
+                        ),
+                        onPressed: () async {
+                          await Share.share(args["url"]);
+                        },
+                      ),
+                  ),
+                  Card(
+                    color: Colors.black,
+                    child: IconButton(
+                        color: Colors.black,
+                        icon: Icon(
+                          Icons.arrow_circle_down,
+                          color: Colors.white,
+                        ),
+                        onPressed: () {
+                          image_Downlader(args["url"] , context);
+                        },
+                      ),
+                  )
+                ],
               ),
               SizedBox(
                 height: 15,
               ),
-              Visibility(
-                visible: isDownloading,
-                child: CircularPercentIndicator(
-                  progressColor: Colors.blue,
-                  radius: 40,
-                  percent: (_progress / 100),
-                  center: Text(
-                    "$_progress",
-                    style: TextStyle(
-                    color: Colors.white
-                  ))
+              Material(
+                type: MaterialType.transparency,
+                child: Center(
+                  child: Text(
+                     args["title"],
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 22
+                    )),
                 ),
-            ),
-          ]),
-          ]
+              ),],
+              ),
+            if(_progress < 100) 
+            Column(
+              children: [
+                Visibility(
+                  visible: isDownloading,
+                  child: Material(
+                    type: MaterialType.transparency,
+                    child: Text(
+                      "Downloading, This could take a few seconds",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 13
+                    )),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Visibility(
+                  visible: isDownloading,
+                  child: CircularPercentIndicator(
+                    progressColor: Colors.blue,
+                    radius: 40,
+                    percent: (_progress / 100),
+                    center: Text(
+                      "$_progress",
+                      style: TextStyle(
+                      color: Colors.white
+                    ))
+                  ),
+              ),
+            ]),
+            if(showDesc) Material(
+              type: MaterialType.transparency,
+              child: Center(
+                child: (
+                  Text(
+                    args["explanation"],
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 13
+                    )
+                  )
+                ),
+              ),
+            ),          
+            ]
+          ),
         ),
       ),
     );
